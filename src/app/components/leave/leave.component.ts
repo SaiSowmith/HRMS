3d515e4f64397fdb28ebfbb2d5c81bea2b1162c7
SaiSowmith <sowmithvajroju@gmail.com>
2018-11-29 13:39:15 +0530

    major changes

--- src/app/components/leave/leave.component.ts
+++ src/app/components/leave/leave.component.ts
@@ -416,6 +416,33 @@ export class ApplyLeave implements OnInit {
         console.log("Error in sendEmailForApplyLeave ", error);
       });
   }
+<<<<<<< HEAD
+=======
+  
+
+  getDays( dDate1, dDate2) {
+    if(dDate1!=null){
+
+      if(dDate2=>dDate1){
+    var iWeeks, iDateDiff, iAdjust = 0;
+    if (dDate2 < dDate1) return -1; // error code if dates transposed
+    var iWeekday1 = dDate1.getDay(); // day of week
+    var iWeekday2 = dDate2.getDay();
+    iWeekday1 = (iWeekday1 == 0) ? 7 : iWeekday1; // change Sunday from 0 to 7
+    iWeekday2 = (iWeekday2 == 0) ? 7 : iWeekday2;
+    if ((iWeekday1 > 5) && (iWeekday2 > 5)) iAdjust = 1; // adjustment if both days on weekend
+    iWeekday1 = (iWeekday1 > 5) ? 5 : iWeekday1; // only count weekdays
+    iWeekday2 = (iWeekday2 > 5) ? 5 : iWeekday2;
+
+    // calculate differnece in weeks (1000mS * 60sec * 60min * 24hrs * 7 days = 604800000)
+    iWeeks = Math.floor((dDate2.getTime() - dDate1.getTime()) / 604800000)
+
+    if (iWeekday1 <= iWeekday2) {
+      iDateDiff = (iWeeks * 5) + (iWeekday2 - iWeekday1)
+    } else {
+      iDateDiff = ((iWeeks + 1) * 5) - (iWeekday1 - iWeekday2)
+    }
+>>>>>>> 735ccec80e06f0103a046cee14e88137c618f62b
 
 
   getDays(dDate1, dDate2) {
